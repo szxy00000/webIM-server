7dce006b6d7329fff0b7c8a1fd4a7a747381af22
罗心成 <luoxincheng@luoxinchengs-MacBook-Air.local>
2023-05-04 22:23:05 +0800

    feat: save

--- app.js
+++ app.js
@@ -1,36 +1,33 @@
 const Koa = require("koa");
+const bodyParser = require('koa-bodyparser');
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const mongo = require("koa-mongo");
 const routers = require('./src/router');
+const cors = require('@koa/cors');
 
 const app = new Koa();
 
+const httpServer = createServer(app.callback());
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*',
+    },
+});
+
+app.use(mongo({
+    uri: 'mongodb+srv://Cluster29796:hJxYm4nA4lpzaRR7@cluster29796.xnopgm4.mongodb.net/?retryWrites=true&w=majority'
+}));
+app.use(cors({
+    origin: '*'
+}));
+app.use(bodyParser());
+app.use(async (ctx, next) => {
+    ctx.io = io;
+    await next();
+})
 routers.forEach(router => {
-  app
-    .use(router.routes())
-    .use(router.allowedMethods());
+    app.use(router.routes()).use(router.allowedMethods());
 })
 
-// app.use(mongo({
-//     uri: 'mongodb+srv://Cluster29796:aOUbCPqRXILpAY1i@cluster29796.xnopgm4.mongodb.net/?retryWrites=true&w=majority'
-// }));
-
-// app.use(async (ctx, next) => {
-//     console.log(ctx, 123123)
-//     ctx.db === ctx.mongo.db('test')
-//     ctx.body = await ctx.db.collection('chat').find().toArray();
-// })
-
-const httpServer = createServer(app.callback());
-// const io = new Server(httpServer, {
-//     cors: {
-//         origin: '*',
-//     },
-//  });
-
-// io.on("connection", (socket) => {
-//   // ...
-// });
-
-httpServer.listen(3000);
+httpServer.listen(7777);
--- src/router/chat.js
+++ src/router/chat.js
@@ -1,9 +1,15 @@
 const Router = require('@koa/router');
 const router = new Router;
 
-router.get('/chat', (ctx, next) => {
-    console.log(213123)
-    ctx.body = 123;
+router.post('/chat', async (ctx, next) => {
+    const msg = ctx.request.body;
+    ctx.io.emit('sync', msg);
+    ctx.mongo.db('test').collection('chat').insertOne(msg);
+    ctx.body = 'success';
+})
+
+router.get('/chatHistory', async (ctx, next) => {
+    ctx.body = await ctx.mongo.db('test').collection('chat').find().toArray();
 })
 
 module.exports = router;
